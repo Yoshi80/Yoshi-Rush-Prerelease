98fe4428a881b823cf06bce2b1f36e01a0b5b9ec
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-06-22 19:20:00 +0200

    added Hyosube and Hell Kappa Daiwet

---
+++ script/c161010027.lua
@@ -0,0 +1,49 @@
+--Hell Kappa Daiwet
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--shuffle to deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.retfilter(c)
+	return c:IsAbleToDeck() and c:IsType(TYPE_MONSTER)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(Card.IsType,tp,0,LOCATION_GRAVE,nil,TYPE_SPELL)
+	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(s.retfilter,tp,0,LOCATION_GRAVE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local ct=Duel.GetMatchingGroupCount(Card.IsType,tp,0,LOCATION_GRAVE,nil,TYPE_SPELL)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.retfilter),tp,0,LOCATION_GRAVE,1,ct,nil)
+	Duel.HintSelection(g)
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		--summon 1 "Kappa Rider" from the GY
+		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+		if #sg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+			Duel.HintSelection(Group.FromCards(tc))
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(160010102) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
