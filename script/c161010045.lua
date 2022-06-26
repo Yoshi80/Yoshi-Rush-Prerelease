5efee3ae4493401d0c0e6159deb8e67ddeed4f2f
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-06-26 10:33:30 +0200

    fixed Eclipse

--- script/c161010045.lua
+++ script/c161010045.lua
@@ -12,19 +12,19 @@ function s.initial_effect(c)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
-function s.desfilter1(c)
+function s.desfilter1(c,tp)
 	return c:IsFaceup() and not c:IsMaximumModeSide() and Duel.IsExistingMatchingCard(s.desfilter2,tp,0,LOCATION_MZONE,1,nil,c:GetLevel())
 end
 function s.desfilter2(c,lv)
 	return c:IsFaceup() and c:IsLevel(lv) and not c:IsMaximumModeSide()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter1,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter1,tp,LOCATION_MZONE,0,1,nil,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_MZONE)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local tc=Duel.SelectMatchingCard(tp,s.desfilter1,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.desfilter1,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
 	if tc then
 		local dg=Duel.SelectMatchingCard(tp,s.desfilter2,tp,0,LOCATION_MZONE,1,1,nil,tc:GetLevel())
 		if #dg>0 then
